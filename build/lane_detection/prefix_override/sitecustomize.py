a5a0531654b205ba3a61b36d155670aded6bec80
ChetanSatpute10 <satputec2006@gmail.com>
2026-03-07 00:59:15 +0530

    okay :(

--- build/lane_detection/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/chetan-satpute/depth_cam/install/lane_detection'
--- build/orbbec_camera/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/chetan-satpute/depth_cam/install/orbbec_camera_msgs/share/orbbec_camera_msgs/package.sh"
